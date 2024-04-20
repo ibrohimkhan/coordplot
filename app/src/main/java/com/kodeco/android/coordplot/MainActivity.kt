23d9823c701295a93ec40f1f59ac587db2c7350c
Ibrohim Kholmatov <ibrohimkhan@gmail.com>
2024-04-20 12:30:33 +0500

    created map composable UI

--- app/src/main/java/com/kodeco/android/coordplot/MainActivity.kt
+++ app/src/main/java/com/kodeco/android/coordplot/MainActivity.kt
@@ -3,12 +3,20 @@ package com.kodeco.android.coordplot
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.kodeco.android.coordplot.ui.theme.MyApplicationTheme
 
 class MainActivity : ComponentActivity() {
@@ -32,6 +40,9 @@ fun PlotSurface() {
         //  This should include a Column composable that
         //  includes a Map, and two MapSlider composables
         //  (one slider for each axis).
+        Column {
+            Map(0f, 0f)
+        }
     }
 
 }
@@ -46,10 +57,19 @@ fun PlotSurfacePreview() {
 
 @Composable
 fun Map(xPercent: Float, yPercent: Float, modifier: Modifier = Modifier) {
-    // TODO fill out the square map here.
-    //  Create a Box composable with a size of 300.dp
-    //  and then create an inner Box composable
-    //  with a clip shape of CircleShape.
+    Box(
+        modifier = Modifier
+            .size(300.dp)
+            .background(color = Color.Blue)
+    ) {
+        Box(
+            modifier = Modifier
+                .size(36.dp)
+                .offset((xPercent * 300 - 18).dp, (yPercent * 300 - 18).dp)
+                .clip(shape = CircleShape)
+                .background(color = Color.Red)
+        )
+    }
 }
 
 @Preview(showBackground = true)
