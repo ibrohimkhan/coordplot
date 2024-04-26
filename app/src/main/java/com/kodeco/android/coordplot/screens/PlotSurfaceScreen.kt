3b8f18f466574644301709ae9d2a6509d4a6f2fb
Ibrohim Kholmatov <ibrohimkhan@gmail.com>
2024-04-26 21:17:20 +0500

    added vertical scroll in PlotSurfaceScreen composable and rememberSaveable to survive state on devise configuration change

--- app/src/main/java/com/kodeco/android/coordplot/screens/PlotSurfaceScreen.kt
+++ app/src/main/java/com/kodeco/android/coordplot/screens/PlotSurfaceScreen.kt
@@ -4,10 +4,12 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableFloatStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -21,13 +23,15 @@ import com.kodeco.android.coordplot.ui.theme.MyApplicationTheme
 
 @Composable
 fun PlotSurfaceScreen() {
-    var xPercentage by remember { mutableFloatStateOf(0.5f) }
-    var yPercentage by remember { mutableFloatStateOf(0.5f) }
+    var xPercentage by rememberSaveable { mutableFloatStateOf(0.5f) }
+    var yPercentage by rememberSaveable { mutableFloatStateOf(0.5f) }
 
     Column(
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center,
-        modifier = Modifier.padding(16.dp)
+        modifier = Modifier
+            .padding(16.dp)
+            .verticalScroll(rememberScrollState())
     ) {
         Map(xPercentage, yPercentage)
         Spacer(modifier = Modifier.padding(0.dp, 16.dp))
